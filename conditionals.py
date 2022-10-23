2cd1c821826fcf2d8d309b3bf7246021cae9ffe7
irah rosete <irah.rosete@outlook.com>
2022-10-24 03:08:52 +1100

    complete Q1 conditionals

---
+++ conditionals.py
@@ -0,0 +1,11 @@
+"""Python module conditionals exercises"""
+
+# Q1. Kate’s cat, Roary, loves catching moths. Write a program that
+# determines whether or not it is time for Roary to catch moths.
+moths_in_house = True
+# moths_in_house = False
+
+if moths_in_house:
+    print("Get the moths!")
+else:
+    print("No threats detected.")
