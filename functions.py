dd0addad5cd1c9e96e002dc9b32ef1585c9c0bed
irah rosete <irah.rosete@outlook.com>
2022-10-31 00:35:52 +1100

    complete Q3 functions

--- functions.py
+++ functions.py
@@ -12,11 +12,23 @@
 
 # Q2. Write a function that accepts one parameter (an integer) and returns True
 # when that parameter is odd and False when that parameter is even.
-def odd_or_even(num):
-    """Returns False when odd or True when even"""
-    if num % 2 == 0:
-        return True
-    else:
-        return False
+# def odd_or_even(num):
+#     """Returns False when odd or True when even"""
+#     if num % 2 == 0:
+#         return True
+#     else:
+#         return False
 
-print(odd_or_even(8))
+# print(odd_or_even(8))
+
+# Q3. Write a function that returns the mean of a list of numbers.
+def mean_number(num_list):
+    """Returns the mean"""
+    total = 0
+    for item in num_list:
+        total += item
+
+    mean = total / (len(num_list))
+    return mean
+
+print(mean_number([1, 2, 3, 4, 5]))
