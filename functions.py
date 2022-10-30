cc272c2410004e8060d3aba010dcc6b645299758
irah rosete <irah.rosete@outlook.com>
2022-10-31 00:31:31 +1100

    complete Q2 functions

--- functions.py
+++ functions.py
@@ -3,9 +3,20 @@
 
 # Q1. Write a function that takes a temperature in fahrenheit and returns
 # the temperature in celsius.
-def fahrenheit_to_celsius(temp):
-    """Convert Fahrenheit to Celsius"""
-    celsius = (temp - 32) * 5 / 9
-    return celsius
+# def fahrenheit_to_celsius(temp):
+#     """Convert Fahrenheit to Celsius"""
+#     celsius = (temp - 32) * 5 / 9
+#     return celsius
 
-print(fahrenheit_to_celsius(32))
+# print(fahrenheit_to_celsius(32))
+
+# Q2. Write a function that accepts one parameter (an integer) and returns True
+# when that parameter is odd and False when that parameter is even.
+def odd_or_even(num):
+    """Returns False when odd or True when even"""
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+print(odd_or_even(8))
