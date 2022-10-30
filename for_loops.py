3372e9a23be310fd41babf485858758f7b1e78ee
irah rosete <irah.rosete@outlook.com>
2022-10-30 20:09:45 +1100

    complete Q3 for loops

--- for_loops.py
+++ for_loops.py
@@ -11,14 +11,25 @@
 #     print(f"{num} * {item} = {num * item}")
 
 # Q2. Ask the user for a number. Use a for loop to sum from 0 to that number.
-num = int(input("Enter a number: "))
-num_list = []
+# num = int(input("Enter a number: "))
+# num_list = []
+# total = 0
+
+# for i in range(num):
+#     num_list.append(i + 1)
+
+# for item in num_list:
+#     total += item
+
+# print(total)
+
+#Q3. Given a list, use a for loop to sum all the numbers in the list.
+random_numbers = [3, 5, 9, 1]
+# random_numbers = [-3, -5, 9, 1]
+# random_numbers = []
+
 total = 0
-
-for i in range(num):
-    num_list.append(i + 1)
-
-for item in num_list:
+for item in random_numbers:
     total += item
 
 print(total)
