0308c48a0d596166c576c50de28bc90bea713ecb
irah rosete <irah.rosete@outlook.com>
2022-10-30 20:05:19 +1100

    complete Q2 for loops

--- for_loops.py
+++ for_loops.py
@@ -1,11 +1,24 @@
 """Python module for loops exercises"""
 
 # Q1. Ask the user for a number. Use a for loop to print the times tables for that number.
+# num = int(input("Enter a number: "))
+# num_list = []
+
+# for i in range(num):
+#     num_list.append(i + 1)
+
+# for item in num_list:
+#     print(f"{num} * {item} = {num * item}")
+
+# Q2. Ask the user for a number. Use a for loop to sum from 0 to that number.
 num = int(input("Enter a number: "))
 num_list = []
+total = 0
 
 for i in range(num):
     num_list.append(i + 1)
 
 for item in num_list:
-    print(f"{num} * {item} = {num * item}")
+    total += item
+
+print(total)
