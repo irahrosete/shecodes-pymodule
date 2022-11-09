44f9cdbeff345ff645322ab8085fbc5ae5768c14
irah rosete <irah.rosete@outlook.com>
2022-11-10 00:20:08 +1100

    complete Q2 csv

--- read_csv.py
+++ read_csv.py
@@ -2,8 +2,16 @@
 # pylint: disable=invalid-name
 import csv
 
-#Q1. Write a program that reads in colours_20_simple.csv and output the colour data.
-with open("csv_files/colours_20.csv", encoding="utf8") as colours_file:
+# Q1. Write a program that reads in colours_20_simple.csv and output the colour data.
+# with open("csv_files/colours_20.csv", encoding="utf8") as colours_file:
+#     reader = csv.reader(colours_file)
+#     for line in reader:
+#         print(f"{line[1].lstrip():<12} {line[2].lstrip():<8} {line[4].lstrip()}")
+
+# Q2. Write a program that reads in colours_20_simple.csv and outputs the colour data in order
+# English, Hex then RGB.
+with open("csv_files/colours_20_simple.csv", encoding="utf8") as colours_file:
     reader = csv.reader(colours_file)
+    next(reader)
     for line in reader:
-        print(f"{line[1].lstrip():<12} {line[2].lstrip():<8} {line[4].lstrip()}")
+        print(f"{line[2]}, Hex: {line[1]}, RGB: {line[0]}")
