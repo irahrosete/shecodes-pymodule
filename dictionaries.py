9ce3cedae2bc0ae991fa266386f13126fa1e65a1
irah rosete <irah.rosete@outlook.com>
2022-11-10 02:15:08 +1100

    complete Q1 dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -6,10 +6,23 @@
 # quantity of each item purchased. Use these two dictionaries to write a program that
 # outputs the cost of each item.
 groceries = {
-"Baby Spinach": 2.78,
-"Hot Chocolate": 3.70,
-"Crackers": 2.10,
-"Bacon": 9.00,
-"Carrots": 0.56,
-"Oranges": 3.08
+  "Baby Spinach": 2.78,
+  "Hot Chocolate": 3.70,
+  "Crackers": 2.10,
+  "Bacon": 9.00,
+  "Carrots": 0.56,
+  "Oranges": 3.08
 }
+
+quantity = {
+  "Baby Spinach": 1,
+  "Hot Chocolate": 3,
+  "Crackers": 2,
+  "Bacon": 1,
+  "Carrots": 4,
+  "Oranges": 2
+}
+
+for item, price in groceries.items():
+    total = quantity[item] * price
+    print(f"{quantity[item]} {item} @ ${price} = ${total:.2f}")
